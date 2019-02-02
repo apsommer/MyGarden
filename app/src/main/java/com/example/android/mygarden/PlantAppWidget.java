003f35d024c0ac7d44e794265bddab54eed9d867
Drew Sommer <andrew.p.sommer@gmail.com>
2019-02-02 11:29:17 -0800

    done

---
+++ app/src/main/java/com/example/android/mygarden/PlantAppWidget.java
@@ -0,0 +1,55 @@
+package com.example.android.mygarden;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.android.mygarden.ui.MainActivity;
+
+// TODO (3): Create a PlantWidgetProvider class that extends AppWidgetProvider and
+// set updateAppWidget to handle clicks and launch MainActivity
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class PlantAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_app_widget);
+
+        // pending intent to be executed by the system when the imageview is clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // bind the pending intent to the imageview
+        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
